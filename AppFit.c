b5762547ab2cf407af87b5babbcf2023d2a694e6
Your Name <you@example.com>
2025-06-06 19:41:09 -0400

    actualizando csv

--- AppFit.c
+++ AppFit.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tdas/list.h"
+#include "tdas/map.h"
+
+typedef struct{
+    short alturaCm ; //altura del usuario en centimetros
+    short pesoKg ; //peso del usuario en kilogramos
+    short edad ;
+    char sexo ; // 'M' masculino, 'F' femenino
+    short objetivo ; // 1:ganar masa muscular, 2:perder grasa, 3:recomposicion corporal
+} Usuario ;
+typedef struct{
+    int valorNutricional ;
+    float proteinas ; //en gramos
+    float carbohidratos ; //en gramos
+    float grasas ; //en gramos
+    float fibra ; //en gramos
+} Alimento ;
+
+void menufitApp() {
+    
+}
+
+int main() {
+
+    return 0 ;
+}
