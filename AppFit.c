80cec301b9de67b92d49077b094e3ed8b77cf581
Your Name <you@example.com>
2025-06-18 21:20:31 -0400

    Funcion leer csv y creacion del mapa de alimentos

--- AppFit.c
+++ AppFit.c
@@ -19,7 +19,50 @@ typedef struct{
     float fibra ; //en gramos
 } Alimento ;
 
-void menufitApp() {
+
+int is_equal_string(const char *str1, const char *str2)
+{ 
+    return strcmp(str1, str2) == 0;
+}
+
+Map *ReadCsv_AddToMap(const char *nombreArchivo)
+{
+    FILE *archivo = fopen(nombreArchivo, "r");
+    if (archivo == NULL) 
+    {
+        perror("Error al abrir el archivo");
+        return NULL;
+    }
+    Map *mapaDeAlimentos = map_create(is_equal_string);
+    char linea[256];
+    fgets(linea, sizeof(linea), archivo); 
+    while (fgets(linea, sizeof(linea), archivo))
+    {
+        char *aux = strtok(linea, ",");
+        if (aux == NULL) continue; 
+
+        char* nombreAlimento = strdup(aux);
+        Alimento *alimento = malloc(sizeof(Alimento));
+        if (alimento == NULL) 
+        {
+            perror("Error al asignar memoria para alimento");
+            free(nombreAlimento);
+            fclose(archivo);
+            return NULL;
+        }
+        alimento->valorNutricional = atoi(strtok(NULL, ","));
+        alimento->carbohidratos = atof(strtok(NULL, ","));
+        alimento->proteinas = atof(strtok(NULL, ","));
+        alimento->grasas = atof(strtok(NULL, ","));
+        alimento->fibra = atof(strtok(NULL, ","));
+        map_insert(mapaDeAlimentos, nombreAlimento, alimento);
+    }
+    fclose(archivo);
+    return mapaDeAlimentos;
+}
+
+void menufitApp() 
+{
     
 }
 
